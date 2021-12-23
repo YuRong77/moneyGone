bc4bd3cbc8ea861843be1ccdf8c98db8db035a93
Yu Rong <gcobc50335@gmail.com>
2021-12-24 01:39:06 +0800

    modify detailPopup

--- src/components/popup/detailPopup.vue
+++ src/components/popup/detailPopup.vue
@@ -9,19 +9,19 @@
           :key="index"
         >
           <div class="directions">
-            <div class="date">建立時間: {{ getDate(item.date) }}</div>
-            <div class="remark">備註: {{ item.remark }}</div>
+            <span>建立時間: {{ getDate(item.date) }}</span>
+            <span>備註: {{ item.remark }}</span>
           </div>
           <div class="info">
-            <div>
-              <div>{{ item.type }}</div>
-              <div>{{ item.name }}</div>
-            </div>
-            <div>{{ item.spend }}</div>
+            <div class="type">{{ item.type }}</div>
+            <div class="name">{{ item.name }}</div>
+            <div class="spend">{{ item.spend }}</div>
           </div>
         </div>
       </div>
-      <button @click="$emit('update:detailPopup', false)">關閉</button>
+      <div class="">
+        <button @click="$emit('update:detailPopup', false)">關閉</button>
+      </div>
     </div>
   </div>
 </template>
@@ -55,22 +55,41 @@ export default {
   background: rgba(0, 0, 0, 0.1);
   .detailCard {
     width: 340px;
-    height: 600px;
-    padding: 30px 20px;
+    height: 630px;
+    padding: 20px 20px;
     border-radius: 15px;
-    background: white;
     h2 {
       font-size: 16px;
       text-align: center;
     }
     .detailList {
       margin: 20px 0;
+      height: 480px;
+      overflow-y: auto;
       .detailItem {
+        border-radius: 5px;
+        margin-bottom: 20px;
+        padding: 10px 15px;
         .directions {
-          display: flex;
+          margin-bottom: 10px;
+          padding-bottom: 5px;
+          span {
+            font-size: 12px;
+            margin-right: 30px;
+          }
         }
         .info {
           display: flex;
+          .type {
+            margin-right: 20px;
+          }
+          .name {
+            font-size: 14px;
+            margin-right: auto;
+          }
+          .spend {
+            font-size: 14px;
+          }
         }
       }
     }
